b6cbfa81672408b8ca9524a02447294dd46962ee
Emil-krarup <emil.krarup@live.dk>
2024-01-10 12:46:59 +0100

    updated servo motor

--- Webserver/Webserver.ino
+++ Webserver/Webserver.ino
@@ -20,19 +20,21 @@ void handleNotFound();
 // Servo motor for simulating office door
 Servo door;
 const int closedDoor = 0;
-const int openedDoor = 90;
+const int openedDoor = 130;
 
 
 void setup() {
   Serial.begin(115200);
   delay(10);
 
+/*
   pinMode(led, OUTPUT);
   digitalWrite(led, 1);
+*/
 // Init servo motor
   pinMode(servoMotorPin, OUTPUT);
   door.attach(servoMotorPin);
-  door.write(0);
+  door.write(closedDoor);
   
   // Connect to WiFi network
   Serial.println();
@@ -62,7 +64,7 @@ void setup() {
 
 
   server.on("/", HTTP_GET, handleRoot);
-  server.on("/LED", HTTP_POST, handleLED);
+  //server.on("/LED", HTTP_POST, handleLED);
   server.on("/DOOR", HTTP_POST, handleDoor);
   server.onNotFound(handleNotFound);
 
@@ -85,43 +87,42 @@ void loop() {
 
 
 void handleDoor() {
-  
-  
-  Serial.println("start handling");
+  //Change this to interact with access card
   (door.read()==0) ? openDoor() : closeDoor() ;
   
   server.sendHeader("Location", "/");    // Add a header to respond with a new location for the browser to go to the home page again
   server.send(303);
-  Serial.println("done handling");
+  Serial.println("Done handling");
 }
 
+//Change this to interact with access card
 void openDoor() {
-  //debug
-  Serial.println("opening door");
-  Serial.println(digitalRead(servoMotorPin));
-
+  Serial.println("TESTING OPEN");
   door.write(openedDoor);
-
-  
-  //debug
-  Serial.println(door.read());
-  Serial.println("Done with opening"); 
 }
 
-
+//Change this to interact with access card
 void closeDoor() {
-   //debug
-  Serial.println("closing door");
-
-
+  Serial.println("TESTING CLOSE");
   door.write(closedDoor);
- 
-  Serial.println(door.read());
-  //debug
-  Serial.println("Done with closing"); 
-  
 }
 
+// Håndtere html siden for vores webserver
+void handleRoot() {  // When URI / is requested, send a web page with a button to toggle the LED
+  server.send(200, "text/html",
+              "<html><title>Internet of Things - Demonstration</title><meta charset=\"utf-8\" \/> \ 
+      </head><body><h1>Velkommen til denne WebServer</h1> \ 
+      <p>Internet of Things (IoT) er \"tingenes Internet\" - dagligdags ting kommer på nettet og får ny værdi. Det kan løse mange udfordringer.</p> \
+      <p>Her kommunikerer du med en webserver på en lille microcontroller af typen Arduino, som i dette tilfælde styrer en digital udgang, som du så igen kan bruge til at styre en lampe, en ventilator, tænde for varmen eller hvad du lyster</p> \
+      <p>Klik på nedenstående knap for at tænde eller slukke LED på port D2</p> \
+      <form action=\"/DOOR\" method=\"POST\"><input type=\"submit\" value=\"Skift tilstand på kontor dør\" style=\"width:500px; height:100px; font-size:24px\"></form> \
+      <p>Med en Arduino ESP8266 kan du lave et have a sjove projekter</p> \
+      <p>Vil du vide mere: Kig på hjemmesiden for uddannelsen : <a href=\"www.dtu.dk/net\">Netværksteknologi og it</a></p> \
+      </body></html>");
+}
+
+/*
+//This is for LED button and Servo motor
 void handleRoot() {  // When URI / is requested, send a web page with a button to toggle the LED
   server.send(200, "text/html",
               "<html><title>Internet of Things - Demonstration</title><meta charset=\"utf-8\" \/> \ 
@@ -134,7 +135,7 @@ void handleRoot() {  // When URI / is requested, send a web page with a button t
       <p>Med en Arduino ESP8266 kan du lave et have a sjove projekter</p> \
       <p>Vil du vide mere: Kig på hjemmesiden for uddannelsen : <a href=\"www.dtu.dk/net\">Netværksteknologi og it</a></p> \
       </body></html>");
-}
+}*/
 
 void handleLED() {                       // If a POST request is made to URI /LED
   digitalWrite(led, !digitalRead(led));  // Change the state of the LED
