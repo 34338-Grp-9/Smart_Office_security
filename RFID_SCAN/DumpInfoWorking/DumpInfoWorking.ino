1bd88d9a0d44b290bb5b5dd03fc75c3e62b48303
Simssek210 <35723271+Simssek210@users.noreply.github.com>
2024-01-15 12:00:43 +0100

    Added firebase and removed some prints

--- RFID_SCAN/DumpInfoWorking/DumpInfoWorking.ino
+++ RFID_SCAN/DumpInfoWorking/DumpInfoWorking.ino
@@ -1,64 +1,30 @@
 /*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read data from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- * 
- * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- * 
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- * 
- * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
- * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
- * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
- * keep the PICCs at reading distance until complete.
- * 
- * @license Released into the public domain.
- * 
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- *
- * More pin layouts for other boards can be found here: https://github.com/miguelbalboa/rfid#pin-layout
- */
-
-/*
 Just a comment to remember where the pins are suppose to be
-
-RST = 5 D1
-SPI ss = 4 D2
+RST = D1
+SPI ss = D2
 SPI MOSI  = D7
 SPI MISO = D6
 SPI SCK = D5
-
-
 */
-
-
+#include <ESP8266Firebase.h>  //Library by https://github.com/Rupakpoddar/ESP8266Firebase
+#include <ESP8266WiFi.h>
+#include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <Servo.h>
 
-#define RST_PIN D1  //D1    // Configurable, see typical pin layout above
-#define SS_PIN D2   //D2 // Configurable, see typical pin layout above
-#define Buzzer D8   //D8
-#define SERVO_PIN 2
-#define PIR_SENSOR D3
+//Our electronics
+#define RST_PIN D1   //D1
+#define SS_PIN D2    //D2
+#define BUZZER D8    //D8
+#define SERVO_PIN 2  //D4
+#define LED D0       //D0
 
-const int LED = D0;
+//Wifi
+#define SSID "Simsek"                                                                        // SSID
+#define PASSWORD "Yavuz112"                                                                  // Password
+#define REFERENCE_URL "https://proj1-2189f-default-rtdb.europe-west1.firebasedatabase.app/"  // Firebase reference URL
+
+
 int LEDSTATE = 0;
 unsigned long previousMillis = 0;
 unsigned long interval = 1000;
@@ -66,19 +32,37 @@ unsigned long interval = 1000;
 
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+Firebase firebase(REFERENCE_URL);  //firebase instance
 Servo servo;
 
 void setup() {
-  Serial.begin(9600);  // Initialize serial communications with the PC
+  Serial.begin(115200);
   SPI.begin();         // Init SPI bus
   mfrc522.PCD_Init();  // Init MFRC522
-  delay(4);            // Optional delay. Some board do need more time after init to be ready, see Readme¨
-  pinMode(Buzzer, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
   servo.attach(SERVO_PIN);
   pinMode(LED, OUTPUT);
   servo.write(0);
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println("Bro, det virker");
+
+  // Connect to WiFi
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to: ");
+  Serial.println(SSID);
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi Connected");
+
+  // Print the IP address
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
@@ -99,31 +83,39 @@ void DOOR_OPEN() {
     return;
   }
 
-  Serial.print("UID tag :");
-  String content = "";
+  String UID = "";
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    UID.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : ""));
+    UID.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-  if (content.substring(1) == "A1 C5 D9 1D") {
+  UID.toUpperCase();
+
+  //defining a string to print out the user of the scanned UID
+  String UIDPrint = firebase.getString("users/" + UID);
+
+  /*
+  ul is what firebase registeres when a tag that is not from the database.
+  the getString calls out our databse to check if the scanned uid is not ul and from the database.
+  */
+  Serial.print("Querying for UID: ");
+  Serial.println(UID);
+  if (firebase.getString("users/" + UID) != "ul") {
     // Card with matching UID
     if (currentMillis - previousMillis >= interval) {
       // save the last time you blinked the LED
       previousMillis = currentMillis;
-      tone(Buzzer, 440, 500);  // Play tone
-      Serial.println("brooo");
+      tone(BUZZER, 440, 500);  // Play tone
+
       // if the LED is off turn it on and vice-versa:
       if (LEDSTATE == LOW) {
         LEDSTATE = HIGH;
       } else {
         LEDSTATE = LOW;
       }
+      //Prints user of the UID
+      Serial.print("User: ");
+      Serial.println(UIDPrint);
       // set the LED with the ledState of the variable:
       digitalWrite(LED, LEDSTATE);
       servo.write(0);  // Make servo go to 0 degrees
@@ -136,6 +128,8 @@ void DOOR_OPEN() {
 
   } else {
     // Card with non-matching UID
-    tone(Buzzer, 200, 500);
+    Serial.print(UID);
+    Serial.println(", Does not exist");
+    tone(BUZZER, 200, 500);
   }
 }
